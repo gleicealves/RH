8ec40a44f85cc6d79301a3eea27b679939184d62
Gleiceara Alves de Azevedo <91923187+gleicealves@users.noreply.github.com>
2022-06-13 13:08:06 -0300

    Add files via upload

---
+++ RH_da_Escola/src/view/Main.java
@@ -0,0 +1,11 @@
+package view;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Digite uma opção: " + "\n");
+        System.out.println("1- Cadastrar novo funcionario.");
+        System.out.println("2- Pesquisar por um funcionario.");
+        System.out.println("3- Mostrar todos os funcionário.");
+    }
+    
+}
